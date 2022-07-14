703f3790eeb48a9798b22ecf787458548e422514
Your Name <you@example.com>
2022-07-14 23:34:31 +0000

    pushing the code

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -16,3 +16,30 @@
 # Step 4:
 # Forward the container port to a host
 
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=prerit18/mlpred
+# Step 2
+# Run the Docker Hub container with kubernetes
+r=$(kubectl get pod mlpred 2> /dev/null;echo $?)
+
+if [ "_$r" == "_1" ] ; then
+    kubectl run mlpred \
+    --image=$dockerpath \
+    --image-pull-policy="Always" \
+    --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
+fi
+
+echo "Hello"
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward mlpred 8000:80
+
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=prerit18/mlpred
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u prerit18
+docker tag mlpred $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
